4d73e47967c78676eba90a1431b746f4e9604fa8
Daman <damanarora724@gmail.com>
2020-06-02 04:50:00 +0530

    Second commit. basic UI already done Almost done with storing and retrieving data to and from a file

---
+++ src/com/trickybhai/todolist/datamodel/TodoData.java
@@ -0,0 +1,84 @@
+package com.trickybhai.todolist.datamodel;
+
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.List;
+
+public class TodoData {
+    private static TodoData instance = new TodoData();
+    private static String filename = "TotolistItems.txt";
+
+    private List<Todoitems> todoitems;
+    private DateTimeFormatter formatter;
+
+    public static TodoData getInstance(){
+        return instance;
+    }
+
+    private TodoData(){
+        formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    }
+
+    public List<Todoitems> getTodoitems() {
+        return todoitems;
+    }
+
+    public void setTodoitems(List<Todoitems> todoitems) {
+        this.todoitems = todoitems;
+    }
+
+    public void loadTodoItems() throws IOException{
+        todoitems = FXCollections.observableArrayList();
+        Path path = Paths.get(filename);
+        BufferedReader br = Files.newBufferedReader(path);
+
+        String input;
+
+        try{
+            while ((input = br.readLine()) != null){
+                String[] itemPieces = input.split("\t");
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dateString = itemPieces[2];
+
+                LocalDate date = LocalDate.parse(dateString, formatter);
+
+                Todoitems todoitems = new Todoitems(shortDescription, details, date);
+                this.todoitems.add(todoitems);
+            }
+        } finally {
+            if (br!=null){
+                br.close();
+            }
+        }
+    }
+
+    public void storeTodoItems() throws IOException{
+        Path path = Paths.get(filename);
+        BufferedWriter bw = Files.newBufferedWriter(path);
+
+        try{
+            Iterator<Todoitems> iter = todoitems.iterator();
+                while(iter.hasNext()){
+                    Todoitems item = iter.next();
+                    bw.write(String.format("%s\t%s\t%s",item.getShortDesciption(), item.getDetails(),
+                    item.getDeadline().format(formatter)));
+                    bw.newLine();
+                }
+        }finally {
+            if (bw!=null){
+                bw.close();
+            }
+        }
+    }
+}
